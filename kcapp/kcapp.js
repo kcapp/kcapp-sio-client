65e8a38ebd12503762544dcb50adf008d09182d2
thordy <thordendre@gmail.com>
2019-03-25 18:41:57 +0100

    Initial version of kcapp sio client

---
+++ kcapp/kcapp.js
@@ -0,0 +1,65 @@
+var denbug = require('debug')('kcapp:client');
+var io = require("socket.io-client");
+
+exports.DART_REIDAR_VENUE_ID = 4;
+
+/**
+ * Handle the leg_finished event, by setting up, and connecting to
+ * the namespace for the next leg
+ * @param {object} data - Data emitted when leg is finished
+ */
+function onLegFinished(data) {
+    debug(`Leg ${data.old_leg_id} finished, setting up new namespace ${data.new_leg_id}`);
+    this.connectLegNamespace(data.new_leg_id, this.legConnectedCallback);
+}
+
+/**
+ * Register a callback for the given event
+ * @param {string} event - Socket IO Event to register callback for
+ * @param {function} callback - Callback when event is emitted
+ */
+exports.on = (event, callback) => {
+    this.socket.on(event, callback);
+}
+
+/**
+ * Connect to the namespace for the given leg
+ * @param {int} id - Leg ID
+ */
+exports.connectLegNamespace = (id, callback) => {
+    var leg = this.legHandler.connect(id, callback);
+    leg.socket.on('leg_finished', onLegFinished.bind(this));
+    this.legs[id] = leg;
+    this.legConnectedCallback = callback;
+}
+
+/**
+ * Connect to the socket
+ */
+exports.connect = (callback) => {
+    this.socket = io(`${this.baseURL}/active`);
+    this.socket.on('connect', (data) => {
+        debug(`Connected to namespace "${this.socket.nsp}" on "${this.baseURL}"`)
+        callback(data);
+    });
+}
+
+/**
+ * Disconnect from the socket
+ */
+exports.disconnect = () => {
+    this.socket.disconnect();
+    debug(`Disconnected from "${this.socket.nsp}"`);
+}
+
+/**
+ * Configure the kcapp module
+ * @param {string} ip - IP of socket.io endpoint
+ * @param {int} port = Port of socket.io endpoint
+ */
+module.exports = (ip, port) => {
+    this.baseURL = `http://${ip}:${port}`;
+    this.legHandler = require('./leg-handler')(this.baseURL);
+    this.legs = {};
+    return this;
+};
---
+++ kcapp/leg-handler.js
@@ -0,0 +1,127 @@
+var denbug = require('debug')('kcapp:leg-handler');
+var io = require("socket.io-client");
+
+var DART_MISS = { value: 0, multiplier: 1 };
+
+/**
+ * Handle a connected event
+ * @param {object} data
+ */
+function onConnected(data) {
+    onScoreUpdate.bind(this)(data);
+    debug(`Connected to /legs/${this.id}`);
+    this.connectCallback(this);
+}
+
+/**
+ * Handle a score_update event
+ * @param {object} data - Data
+ */
+function onScoreUpdate(data) {
+    this.currentPlayerId = data.leg.current_player_id;
+    this.leg = data.leg;
+    this.players = data.players;
+
+    // Reset variables keeping track of throws
+    this.throws = [];
+    this.dartsThrown = 0;
+}
+
+/**
+ * Register a callback for the given event
+ * @param {string} event - Socket IO Event to register callback for
+ * @param {function} callback - Callback when event is emitted
+ */
+exports.on = (event, callback) => {
+    this.socket.on(event, callback);
+}
+
+/**
+ * Emit a throw, indicating that the given dart was just thrown
+ *
+ * @param {object} dart - Dart thrown
+ * @param {boolean} isUndo - True to undo last throw
+ */
+exports.emitThrow = (dart, isUndo) => {
+    if (isUndo) {
+        this.dartsThrown--;
+        this.throws.splice(-1, 1);
+    } else {
+        this.dartsThrown++;
+        this.throws.push(dart);
+    }
+    var payload = {
+        current_player_id: this.currentPlayerId,
+        score: dart.score,
+        multiplier: dart.multiplier,
+        darts_thrown: this.dartsThrown,
+        is_undo: isUndo
+    }
+    this.socket.emit('possible_throw', payload);
+    // TODO Auto throw after 3 darts?
+}
+
+/**
+ * Emit a visit, this will confirm the visit, and write all values to database
+ */
+exports.emitVisit = () => {
+    var payload = {
+        player_id: this.currentPlayerId,
+        leg_id: this.id,
+        first_dart: DART_MISS,
+        second_dart: DART_MISS,
+        third_dart: DART_MISS
+    };
+
+    if (this.throws[0]) {
+        var first = this.throws[0];
+        payload.first_dart = { value: first.score, multiplier: first.multiplier };
+    }
+    if (this.throws[1]) {
+        var second = this.throws[1];
+        payload.second_dart = { value: second.score, multiplier: second.multiplier };
+    }
+    if (this.throws[2]) {
+        var third = this.throws[2];
+        payload.third_dart = { value: third.score, multiplier: third.multiplier };
+    }
+
+    var json = JSON.stringify(payload);
+    debug(`Emitting visit ${json} to ${this.socket.nsp}`);
+    this.socket.emit('throw', json);
+}
+
+/**
+ * Connect to namespace of the given leg
+ * @param {int} id - Leg ID
+ */
+exports.connect = (id, callback) => {
+    this.id = id;
+    this.connectCallback = callback;
+    this.socket = io(`${this.baseURL}/legs/${id}`);
+
+    // Update leg, players, etc when score is updated
+    this.socket.on('connect', (data) => {
+        this.socket.emit('join', 'Client Connecting');
+    });
+    this.socket.on('connected', onConnected.bind(this));
+    this.socket.on('score_update', onScoreUpdate.bind(this));
+    return this;
+}
+
+/**
+ * Disconnect from the socket
+ */
+exports.disconnect = () => {
+    this.socket.disconnect();
+    debug(`Disconnected from "${this.socket.nsp}"`);
+}
+
+/**
+ * Configure the leg-handler
+ * @param {string} baseURL - BaseURL of socket.io endpoint
+ */
+module.exports = (baseURL) => {
+    this.baseURL = baseURL;
+    return this;
+}
